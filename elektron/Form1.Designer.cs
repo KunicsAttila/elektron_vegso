fb9c82ec126f7fa8609956ca524b944c597ca9c1
Kunics.Attila <Kunics.Attila@C16-02>
2024-11-28 21:14:55 +0100

    Add project files.

---
+++ elektron/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿
+namespace elektron
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Poz = new System.Windows.Forms.PictureBox();
+            this.Akku = new System.Windows.Forms.PictureBox();
+            this.Feszmero = new System.Windows.Forms.PictureBox();
+            this.Izzo = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonUp = new System.Windows.Forms.Button();
+            this.buttonDown = new System.Windows.Forms.Button();
+            this.alsoVezeto = new System.Windows.Forms.PictureBox();
+            this.felsoVezeto = new System.Windows.Forms.PictureBox();
+            this.jobbVezeto = new System.Windows.Forms.PictureBox();
+            this.Neg = new System.Windows.Forms.PictureBox();
+            this.balVezeto = new System.Windows.Forms.PictureBox();
+            this.passedLabel = new System.Windows.Forms.Label();
+            this.elektron = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Poz)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Akku)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Feszmero)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Izzo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.alsoVezeto)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.felsoVezeto)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.jobbVezeto)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Neg)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.balVezeto)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.elektron)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Poz
+            // 
+            this.Poz.Location = new System.Drawing.Point(650, 320);
+            this.Poz.Name = "Poz";
+            this.Poz.Size = new System.Drawing.Size(26, 21);
+            this.Poz.TabIndex = 0;
+            this.Poz.TabStop = false;
+            // 
+            // Akku
+            // 
+            this.Akku.Location = new System.Drawing.Point(639, 337);
+            this.Akku.Name = "Akku";
+            this.Akku.Size = new System.Drawing.Size(100, 76);
+            this.Akku.TabIndex = 1;
+            this.Akku.TabStop = false;
+            // 
+            // Feszmero
+            // 
+            this.Feszmero.Location = new System.Drawing.Point(301, 23);
+            this.Feszmero.Name = "Feszmero";
+            this.Feszmero.Size = new System.Drawing.Size(95, 119);
+            this.Feszmero.TabIndex = 3;
+            this.Feszmero.TabStop = false;
+            // 
+            // Izzo
+            // 
+            this.Izzo.BackColor = System.Drawing.Color.Yellow;
+            this.Izzo.Location = new System.Drawing.Point(120, 44);
+            this.Izzo.Name = "Izzo";
+            this.Izzo.Size = new System.Drawing.Size(51, 80);
+            this.Izzo.TabIndex = 4;
+            this.Izzo.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(334, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "12V";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonUp
+            // 
+            this.buttonUp.Location = new System.Drawing.Point(311, 63);
+            this.buttonUp.Name = "buttonUp";
+            this.buttonUp.Size = new System.Drawing.Size(75, 23);
+            this.buttonUp.TabIndex = 6;
+            this.buttonUp.Text = "^";
+            this.buttonUp.UseVisualStyleBackColor = true;
+            this.buttonUp.Click += new System.EventHandler(this.buttonUp_Click);
+            // 
+            // buttonDown
+            // 
+            this.buttonDown.Location = new System.Drawing.Point(311, 101);
+            this.buttonDown.Name = "buttonDown";
+            this.buttonDown.Size = new System.Drawing.Size(75, 23);
+            this.buttonDown.TabIndex = 7;
+            this.buttonDown.Text = "V";
+            this.buttonDown.UseVisualStyleBackColor = true;
+            this.buttonDown.Click += new System.EventHandler(this.buttonDown_Click);
+            // 
+            // alsoVezeto
+            // 
+            this.alsoVezeto.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.alsoVezeto.Location = new System.Drawing.Point(138, 320);
+            this.alsoVezeto.Name = "alsoVezeto";
+            this.alsoVezeto.Size = new System.Drawing.Size(517, 2);
+            this.alsoVezeto.TabIndex = 8;
+            this.alsoVezeto.TabStop = false;
+            // 
+            // felsoVezeto
+            // 
+            this.felsoVezeto.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.felsoVezeto.Location = new System.Drawing.Point(138, 92);
+            this.felsoVezeto.Name = "felsoVezeto";
+            this.felsoVezeto.Size = new System.Drawing.Size(581, 2);
+            this.felsoVezeto.TabIndex = 9;
+            this.felsoVezeto.TabStop = false;
+            // 
+            // jobbVezeto
+            // 
+            this.jobbVezeto.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.jobbVezeto.Location = new System.Drawing.Point(717, 92);
+            this.jobbVezeto.Name = "jobbVezeto";
+            this.jobbVezeto.Size = new System.Drawing.Size(2, 230);
+            this.jobbVezeto.TabIndex = 10;
+            this.jobbVezeto.TabStop = false;
+            // 
+            // Neg
+            // 
+            this.Neg.Location = new System.Drawing.Point(703, 320);
+            this.Neg.Name = "Neg";
+            this.Neg.Size = new System.Drawing.Size(26, 21);
+            this.Neg.TabIndex = 11;
+            this.Neg.TabStop = false;
+            // 
+            // balVezeto
+            // 
+            this.balVezeto.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.balVezeto.Location = new System.Drawing.Point(138, 92);
+            this.balVezeto.Name = "balVezeto";
+            this.balVezeto.Size = new System.Drawing.Size(2, 230);
+            this.balVezeto.TabIndex = 12;
+            this.balVezeto.TabStop = false;
+            // 
+            // passedLabel
+            // 
+            this.passedLabel.AutoSize = true;
+            this.passedLabel.Location = new System.Drawing.Point(672, 36);
+            this.passedLabel.Name = "passedLabel";
+            this.passedLabel.Size = new System.Drawing.Size(90, 13);
+            this.passedLabel.TabIndex = 13;
+            this.passedLabel.Text = "Áthaladt elektron:";
+            // 
+            // elektron
+            // 
+            this.elektron.BackColor = System.Drawing.Color.Black;
+            this.elektron.Location = new System.Drawing.Point(712, 256);
+            this.elektron.Name = "elektron";
+            this.elektron.Size = new System.Drawing.Size(14, 13);
+            this.elektron.TabIndex = 14;
+            this.elektron.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.elektron);
+            this.Controls.Add(this.passedLabel);
+            this.Controls.Add(this.balVezeto);
+            this.Controls.Add(this.Neg);
+            this.Controls.Add(this.jobbVezeto);
+            this.Controls.Add(this.felsoVezeto);
+            this.Controls.Add(this.alsoVezeto);
+            this.Controls.Add(this.buttonDown);
+            this.Controls.Add(this.buttonUp);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Izzo);
+            this.Controls.Add(this.Feszmero);
+            this.Controls.Add(this.Akku);
+            this.Controls.Add(this.Poz);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Poz)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Akku)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Feszmero)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Izzo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.alsoVezeto)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.felsoVezeto)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.jobbVezeto)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Neg)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.balVezeto)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.elektron)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Poz;
+        private System.Windows.Forms.PictureBox Akku;
+        private System.Windows.Forms.PictureBox Feszmero;
+        private System.Windows.Forms.PictureBox Izzo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonUp;
+        private System.Windows.Forms.Button buttonDown;
+        private System.Windows.Forms.PictureBox alsoVezeto;
+        private System.Windows.Forms.PictureBox felsoVezeto;
+        private System.Windows.Forms.PictureBox jobbVezeto;
+        private System.Windows.Forms.PictureBox Neg;
+        private System.Windows.Forms.PictureBox balVezeto;
+        private System.Windows.Forms.Label passedLabel;
+        private System.Windows.Forms.PictureBox elektron;
+    }
+}
+
---
+++ elektron/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*A feladat hogy szimuláljuk ahogy az elektronok végigmennek a vezetéken. Az elektron az akkumulátor negatív feléről indul a pozitív felé. Az áramkörben van egy feszültésg szabályzó és egy fogyasztó. A feszültség szabályzóig konstans sebességgel haladnak az elektronok. A feszültség szabályzó az utána lévő részre beállítja az elektron sebességét.
+Az akkumulátor 24 Voltos.
+Az alapértelmezett feszültség érték 12 Volt amit egyesével lehet fel és le módosítani.
+A maximális feszültség nem lehet nagyobb az akku feszültségénél. 
+Miután egy elektron áthaladt a feszültség szabályzón, a sebessége változatlan marad.
+A sebességet arányosan kell beállítani. Ha 12 Volton megy az elektron, akkor a 24 Voltos sebesség 50 százalékával kell haladnia.
+A mozgás timer intervallumja fixen 16ms.
+Van egy számláló ami visszajelzést nyújt arról hány elektron jutott vissza az akkuba.
+Az elektronok fél másodpercenként jönnek létre*/
+
+
+namespace elektron
+{
+    public partial class Form1 : Form
+    {
+        Timer elektronTimer = new Timer();
+        int VoltSize = 12;
+        int Xirany = 0;
+        int Yirany = -1;
+        int elektronSpeed = 0;
+        int upVoltage = 24;
+        int downVoltage = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Feszultseg";
+            Start();
+        }
+        void Start()
+        {
+            elektronTimer.Interval = 24;
+            moveEvent();
+            //FeszultsegEvent();
+            elektronTimer.Start();
+            //elektronTimer.Tick += elektronSpeed;
+
+        }
+        void moveEvent()
+        {
+            elektronTimer.Interval = 24;
+            elektronTimer.Start();
+            /*elektronTimer.Tick += (s, e) =>
+            {
+                buttonUp.Click += upVoltage;
+                buttonDown.Click += downVoltage;
+            };*/
+        }
+        /*void FeszultsegEvent()
+        {
+            upVolt();
+            downVolt();
+            eSpeed();
+        }*/
+        void eSpeed(object s, EventArgs e)
+        {
+            //  felfelé
+            if (Xirany == 0 && Yirany == -1)
+            {
+                if (elektron.Top > alsoVezeto.Top + VoltSize)
+                {
+                    elektron.Top -= VoltSize;
+                }
+                else
+                {
+                    elektron.Top = alsoVezeto.Top - 2;
+                    Xirany = -1;
+                    Yirany = 0;
+                }
+
+            }
+            //  lefelé 
+            else if (Xirany == 0 && Yirany == 1)
+            {
+                if (elektron.Top < alsoVezeto.Bottom - elektron.Height - VoltSize)
+                {
+                    elektron.Top += VoltSize;
+                }
+                else
+                {
+                    elektron.Top = alsoVezeto.Bottom - elektron.Height + 2;
+                    Xirany = 1;
+                    Yirany = 0;
+                }
+            }
+
+            //  balra
+            else if (Xirany == -1 && Yirany == 0)
+            {
+                if (elektron.Left > felsoVezeto.Left + VoltSize)
+                {
+                    elektron.Left -= VoltSize;
+                }
+                else
+                {
+                    elektron.Left = felsoVezeto.Left - 2;
+                    Xirany = 0;
+                    Yirany = 1;
+                    /*eSpeed += 1;
+                    passedLabel.Text = $"Áthaladt elektron:: {eSpeed}";*/
+                }
+            }
+            //  jobbra
+            else if (Xirany == 1 && Yirany == 0)
+            {
+                if (elektron.Left < jobbVezeto.Right - elektron.Width - VoltSize)
+                {
+                    elektron.Left += VoltSize;
+                    if (elektron.Left >= alsoVezeto.Right + 2)
+                    {
+                        elektron.Visible = false;
+                    }
+                }
+                else
+                {
+                    elektron.Left = jobbVezeto.Right - elektron.Width + 2;
+                    Xirany = 0;
+                    Yirany = -1;
+                    elektron.Visible = true;
+                }
+            }
+        }
+
+        private void buttonUp_Click(object sender, EventArgs e)
+        {
+            if (VoltSize < 24)
+            {
+                VoltSize++;
+                Feszmero.Text = VoltSize.ToString() + "V";
+            }
+        }
+
+        private void buttonDown_Click(object sender, EventArgs e)
+        {
+            if (VoltSize > 1)
+            {
+                VoltSize--;
+                Feszmero.Text = VoltSize.ToString() + "V";
+            }
+        }
+    }
+}
